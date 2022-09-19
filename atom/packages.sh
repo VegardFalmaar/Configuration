2293824af1ca29b5a859b7fefbe709e1235e7146
Vegard Falmår <vegard.falmaar@me.com>
2022-09-19 12:49:43 +0200

    Runable script for installation of Atom packages

---
+++ atom/packages.sh
@@ -0,0 +1,8 @@
+# each one can be installed from cml with
+# apm install <package-name>
+
+apm install language-latex
+apm install latex               # by thomasjo, for compiling
+apm install latexer             # by Focus, for autocompletion
+apm install minimap
+apm install file-icons
