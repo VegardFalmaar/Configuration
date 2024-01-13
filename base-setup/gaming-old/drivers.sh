2fa3a1533e7f5ce7894de66530b532feb3682609
Vegard Falmår <vegard.falmaar@me.com>
2024-01-13 23:43:46 +0100

    Add old commands for packages for gaming

---
+++ base-setup/gaming-old/drivers.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y nvidia-driver-470 libvulkan1 libvulkan1:i386
+
+echo ### Please reboot
---
+++ base-setup/gaming-old/lutris-etc.sh
@@ -0,0 +1,5 @@
+echo ### Installing game stuff
+sudo apt install steam wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
+sudo add-apt-repository ppa:lutris-team/lutris
+sudo apt update
+sudo apt install lutris
