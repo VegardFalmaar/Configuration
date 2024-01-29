fdd95a7b18bbcb85b7e6c522f904209eeb5b56a6
Vegard Falmår <vegard.falmaar@me.com>
2024-01-29 15:55:24 +0100

    Edits to the sublime setup

--- base-setup/step3.sh
+++ base-setup/step3.sh
@@ -1,6 +1,6 @@
 msg_fname=~/Documents/installation-messages.txt
 
-### clone repos
+### clone essential repos
 mkdir -p ~/Git
 git clone git@github.com:VegardFalmaar/Books.git ~/Git/Books
 git clone git@github.com:VegardFalmaar/Configuration.git ~/Git/Configuration
@@ -8,18 +8,26 @@ git clone git@github.com:VegardFalmaar/FalmaarTools.git ~/Git/FalmaarTools
 git clone git@github.com:VegardFalmaar/interesting_code.git ~/Git/interesting_code
 git clone git@github.com:VegardFalmaar/scripts.git ~/Git/scripts
 
+### append additions to .bashrc
 echo ". ~/Git/Configuration/bash/bashrc_additions" >> ~/.bashrc
+
+### create symlinc to tmux config
 # ln -s ~/Git/Configuration/tmux/tmux_laptop.conf ~/.tmux.conf
 ln -s ~/Git/Configuration/tmux/tmux_desktop.conf ~/.tmux.conf
+
+### set up vim
 mkdir -p ~/.vim/swap/
 ln -s ~/Git/Configuration/vim/vimrc ~/.vimrc
-ln -s ~/Git/Configuration/pylintrc ~/.pylintrc
-ln -s ~/Git/Configuration/sublime-text/snippets ~/.config/sublime-text/Packages/User/snippets
-ln -s ~/Git/Configuration/sublime-text/Dictionaries ~/.config/sublime-text/Packages/User/Dictionaries
-ln -s ~/Git/Configuration/sublime-text/keybindings.sublime-keymap ~/.config/sublime-text/Packages/User/Default\ \(Linux\).sublime-keymap
-ln -s ~/Git/Configuration/ssh_config ~/.ssh/config
-echo "Dotfiles are set up. Please remove color prompt from ~/.bashrc" >> $msg_fname
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "VimPlug is set up. Remember to run PlugInstall" >> $msg_fname
+
+### sublime-text
+ln -s ~/Git/Configuration/sublime-text/Snippets ~/.config/sublime-text/Packages/Snippets
+ln -s ~/Git/Configuration/sublime-text/Dictionaries ~/.config/sublime-text/Packages/Dictionaries
+ln -s ~/Git/Configuration/sublime-text/keybindings.sublime-keymap ~/.config/sublime-text/Packages/User/Default\ \(Linux\).sublime-keymap
+
+### misc
+ln -s ~/Git/Configuration/pylintrc ~/.pylintrc
+ln -s ~/Git/Configuration/ssh_config ~/.ssh/config
+echo "Dotfiles are set up. Please remove color prompt from ~/.bashrc" >> $msg_fname
---
+++ sublime-text/README.md
@@ -0,0 +1,33 @@
+Key Bindings
+====
+
+
+LaTeX specific
+----
+
+- Clean build dir.: `Ctrl + L -- Backspace`
+- Change surrounding env: `Ctrl + l -- Ctrl + Shift + N`
+- Toggle * in surrounding env: `Ctrl + l -- Ctrl + Shift + 8`
+
+
+Snippets
+====
+
+The following snippets are created.
+Custom snippets all start with "s" to avoid collisions with built-in keywords.
+
+Snippets are expanded with `Shift + Enter`.
+
+
+Text Mode
+----
+
+- Insert figure: "sfig"
+- Inline equation: "sin"
+- Block equation: "eq"
+
+
+Math Mode
+----
+
+- Fraction: "sfr"
